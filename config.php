1d0ad2a458ca54fbd1be4e62b126fad23b11d56c
MerrittofD4 <merritt@d4webdesign.com>
2017-03-13 16:36:39 -0700

    1.1.1
    Added customsearch

--- config.php
+++ config.php
@@ -12,15 +12,17 @@ To enable a feature, just set the value to "true"
 ------------------------------------------------------*/
 
 $d4pizza_config = array(
-	'testimonial' 		=> false,
+	
 	'brand' 			=> false,
-	'staffmember' 		=> false,
 	'button' 			=> false,
-	'getpost' 			=> false,
 	'carousel' 			=> false,
+	'customsearch'		=> false,
+	'fonticons'			=> false,	
+	'getpost' 			=> false,	
 	'googlemap' 		=> false,
 	'reanimator' 		=> false,
 	'slidemenu' 		=> false,
-	'fonticons'			=> false,
+	'staffmember' 		=> false,	
+	'testimonial' 		=> false,
 );
 ?>
